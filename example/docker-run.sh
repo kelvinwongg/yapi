1f2b3be06916b2d2d17dc268bc89387a3d9855cd
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-11-09 16:35:10 +0800

    Remove example from packagist

--- example/docker-run.sh
+++
@@ -1,5 +0,0 @@
-# Use this docker command to start development server
-# docker run -d -p 8081:80 --name yapi -v "$PWD":/var/www/html php:7.4.25-apache
-
-# Use this docker command to start development server with XDebug enabled
-docker run -d -p 8081:80 --name yapi -v "$PWD":/var/www/html --entrypoint="/var/www/html/example/docker-entrypoint.sh" php:7.4.25-apache
--- example/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$yapi = new Kelvinwongg\Yapi\Yapi();
