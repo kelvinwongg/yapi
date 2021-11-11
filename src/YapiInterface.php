bd6659c7a308885a79da97f82cafcaba74a5c296
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-11-11 17:30:05 +0800

    YapiInterface draft

--- src/YapiInterface.php
+++ src/YapiInterface.php
@@ -2,6 +2,19 @@
 
 namespace Kelvinwongg\Yapi;
 
+use Kelvinwongg\Yapi\Core\RequestInterface;
+use Kelvinwongg\Yapi\Core\ResponseInterface;
+use Kelvinwongg\Yapi\Core\ParserInterface;
+use Kelvinwongg\Yapi\Core\SystemCheckerInterface;
+
 interface YapiInterface
 {
+	public function init();
+	public function handleRequest(): RequestInterface;
+	public function parseYaml(ParserInterface $parser): bool;
+	// public function systemCheck(SystemCheckerInterface $checker): bool;
+	public function beforeHook(): ResponseInterface;
+	public function crudOperation(): ResponseInterface;
+	public function afterHook(): ResponseInterface;
+	public function handleResponse(): ResponseInterface;
 }
