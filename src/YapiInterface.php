96371ebacd1086052878a2ca18ace41afd8c9592
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-11-19 15:02:35 +0800

    YapiInterface draft v2 notes

--- src/YapiInterface.php
+++ src/YapiInterface.php
@@ -10,22 +10,55 @@ interface YapiInterface
 {
 	/**
 	 * init
-	 * run at __construct, process the normal flow,
-	 * if user provide a yaml file
+	 * 
+	 * Run at __construct function.
+	 * Process the YAPI normal flow if a YAML document presents.
 	 *
-	 * @param  File $file
-	 * @return self
+	 * @param  File $file The YAML document.
+	 * @return self Return itself for manual flow.
 	 */
 	public function init(File $file): self;
 
 	/**
 	 * handleRequest
 	 * 
+	 * Handle inbound request.
+	 * Do the CRUD operation and/or before/after hooks if exists.
+	 * 
 	 * @param  mixed $request
 	 * @return ResponseInterface
 	 */
 	public function handleRequest(RequestInterface $request): ResponseInterface;
+	
+	/**
+	 * createDatabase
+	 * 
+	 * Create the database schema based on the YAML document.
+	 * Return boolean on success/failure.
+	 *
+	 * @return boolean
+	 */
 	public function createDatabase(): bool;
+
+	/**
+	 * checkDatabase
+	 * 
+	 * Check the database schema based on the YAML document.
+	 * Without any actual change to the database (dry run).
+	 * Return boolean on success/failure.
+	 *
+	 * @return boolean
+	 */
 	public function checkDatabase(): bool;
+
+	/**
+	 * checkYaml
+	 * 
+	 * Check the integrity of the YAML document.
+	 * Without any actual change to the database (dry run).
+	 * Return boolean on success/failure.
+	 *
+	 * @return boolean
+	 */
 	public function checkYaml(): bool;
 }
