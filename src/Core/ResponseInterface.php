9be4cb422daa7fbba143bfbace177bac10e13bab
kelvinwong <KelvinYH_Wong@cncbinternational.com>
2022-08-15 18:10:34 +0800

    ResponseInterface draft

--- src/Core/ResponseInterface.php
+++ src/Core/ResponseInterface.php
@@ -2,6 +2,17 @@
 
 namespace Kelvinwongg\Yapi\Core;
 
+/**
+ * ResponseInterface
+ * 
+ * This class represent the outbound http response,
+ * and all the functions to prepare it to be run in YAPI.
+ * - setStatusCode
+ * - addHeader
+ * - setContentType
+ * - redirect
+ */
 interface ResponseInterface
 {
+
 }
